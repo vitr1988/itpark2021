bc7cd09a49c42d7d48b7b41c626f442b811a0b59
v.ivanov <vivanov@vtb.ru>
2022-04-24 20:40:35 +0400

    lesson44: Мониторинг

---
+++ spring-data-rest/src/main/java/lesson43/config/MetricConfiguration.java
@@ -0,0 +1,17 @@
+package lesson43.config;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class MetricConfiguration {
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
+}
